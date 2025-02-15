f88a4c321b415b245956371482f1b091611b9221
Victoires <94651953+Victoire-s@users.noreply.github.com>
2025-02-15 22:19:45 +0100

    feat(add): Usage of lombok and add of notificationTime in User.java

--- src/main/java/com/esgi/leitner/domain/service/UserService.java
+++ src/main/java/com/esgi/leitner/domain/service/UserService.java
@@ -2,21 +2,27 @@ package com.esgi.leitner.domain.service;
 
 import com.esgi.leitner.domain.model.Token;
 import com.esgi.leitner.domain.model.User;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class UserService {
-    public User createUser(User user){
+
+    public User createUser(User user) {
         return null;
     }
 
-    Optional<User> getUserById(String id) {
+    public Optional<User> getUserById(String id) {
         return null;
     }
+
     public List<User> getAllUsers() {
         return null;
     }
+
     public User updateUser(User user) {
         return null;
     }
@@ -27,8 +33,26 @@ public class UserService {
     public Token generateTokenForUser(User user) {
         return null;
     }
-    public User cooldownCardUser(User user){
+
+    public User cooldownCardUser(User user) {
         return null;
     }
 
+    /**
+     * Update the date where the user will receive the notification
+     *
+     * @param userId the user id
+     * @param notificationTime the new reception date of the notification
+     * @return the user updated
+     * @throws RuntimeException if user not found.
+     */
+    public User updateNotificationTime(String userId, LocalTime notificationTime) {
+        Optional<User> userOpt = getUserById(userId);
+        if (userOpt.isPresent()) {
+            User user = userOpt.get();
+            user.setNotificationTime(notificationTime);
+            return updateUser(user);
+        }
+        throw new RuntimeException("User not found : " + userId);
+    }
 }
